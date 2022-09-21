cd3abf595674dfe0563a786b79cd7035085c3832
azatseyrek <azatseyrek@hotmail.com>
2022-09-21 16:54:28 +0300

    optimazing the app completed

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+import Image from 'next/image';
 import Link from 'next/link';
 import AddressIcon from '../icons/AddressIcon';
 import ArrowRightIcon from '../icons/ArrowRight-icon';
@@ -18,7 +19,7 @@ const EventItem = (props) => {
   const exploreLink = `/events/${id}`;
   return (
     <li className={classes.item}>
-      <img src={'/' + image} alt={title} />
+      <Image src={'/' + image} alt={title} width={250} height={160} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
-import EventList from '../components/events/EventList';
+import Head from 'next/head';
 import { getFeaturedEvents } from '../helper/api-util';
+import EventList from '../components/events/EventList';
 
 const HomePage = (props) => {
   const { events } = props;
 
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta name="description" content="Find a lot of greate events" />
+      </Head>
       <EventList items={events} />
     </div>
   );
